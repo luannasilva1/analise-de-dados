dc4346dbe244084101526b3302c6863d284d08e7
Vinicius Simas <viniciussimaslima19@gmail.com>
2025-06-02 19:35:46 -0300

    modelo fisico certo

--- modelofisico.sql
+++ modelofisico.sql
@@ -1,8 +1,9 @@
+%sql
 CREATE DATABASE IF NOT EXISTS consumo_agua;
 USE consumo_agua;
 
 CREATE TABLE IF NOT EXISTS cliente (
-  id_cliente BIGINT PRIMARY KEY,
+  id_cliente BIGINT,
   nome STRING,
   cidade STRING,
   bairro STRING,
@@ -10,21 +11,19 @@ CREATE TABLE IF NOT EXISTS cliente (
 );
 
 CREATE TABLE IF NOT EXISTS conta_consumo (
-  id_conta BIGINT PRIMARY KEY,
+  id_conta BIGINT,
   id_cliente BIGINT,
   mes_referencia STRING,
   data_vencimento DATE,
   consumo_m3 DOUBLE,
   valor_conta DOUBLE,
   valor_imposto DOUBLE,
-  tempo_atraso_dias INT,
-  FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+  tempo_atraso_dias INT
 );
 
 CREATE TABLE IF NOT EXISTS corte (
-  id_corte BIGINT PRIMARY KEY,
+  id_corte BIGINT,
   id_cliente BIGINT,
   data_corte DATE,
-  status_corte STRING,
-  FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+  status_corte STRING
 );
